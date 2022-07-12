7c53b3d92a329dfdbeaff6dddf3c5726b6769e02
Rasul <89188458381@mail.ru>
2022-07-12 09:41:31 +0300

    add squer of number

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 	По двум заданным числам проверять является ли первое квадратом второго
+
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a*a==b)
+{
+    System.Console.WriteLine("b is squer a");
+}
+else
+{
+    System.Console.WriteLine("b is't squer a");
+}
+if(a==b*b)
+{
+    System.Console.WriteLine("a is squer b");
+}
+else
+{
+    System.Console.WriteLine("a is't squer b");
+}
