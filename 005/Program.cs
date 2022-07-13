ba17bfbfe935c6d0601019c8ecdad3fe7b12ab9d
Rasul <89188458381@mail.ru>
2022-07-13 10:48:48 +0300

    add Найти максимальное из трех чисел

---
+++ 005/Program.cs
@@ -0,0 +1,16 @@
+﻿// 5.	Найти максимальное из трех чисел
+
+int a = 23;
+int b = 450;
+int c = 234;
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+System.Console.WriteLine(max);
