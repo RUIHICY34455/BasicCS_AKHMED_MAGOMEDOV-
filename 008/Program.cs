068b2c0ae5cad4df8d8666ce8a93a93da2ee60fa
Rasul <89188458381@mail.ru>
2022-07-13 19:33:49 +0300

    add Показать числа от -N до N

---
+++ 008/Program.cs
@@ -0,0 +1,10 @@
+﻿// 8.	Показать числа от -N до N
+
+int[] array = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+int index = 0;
+int length = array.Length;
+while (index < length)
+{
+    System.Console.WriteLine(array[index]);
+    index++;
+}
