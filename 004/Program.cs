d7d05a3e04c0c2828920377718898fe3bdd34e70
Rasul <89188458381@mail.ru>
2022-07-13 10:40:32 +0300

    add 004

---
+++ 004/Program.cs
@@ -0,0 +1,34 @@
+﻿// 4.	По заданному номеру дня недели вывести его название
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+if (a == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+if (a == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+if (a == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+if (a == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+if (a == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+if (a == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+if (a == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}
+
