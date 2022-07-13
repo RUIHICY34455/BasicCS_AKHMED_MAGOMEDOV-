2794eda76e0c662163541ff2df07718ad5a0eb52
Rasul <89188458381@mail.ru>
2022-07-13 18:23:03 +0300

    add Написать программу вычисления значения функции y = f(a)

---
+++ 006/Program.cs
@@ -0,0 +1,15 @@
+﻿// 6.	Написать программу вычисления значения функции y = f(a)
+
+int y, a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+if (a == 0)
+{
+    y = 0;
+}
+else
+{
+    y = 2 * Math.Abs(a) + 10;
+}
+
+System.Console.WriteLine(y);
