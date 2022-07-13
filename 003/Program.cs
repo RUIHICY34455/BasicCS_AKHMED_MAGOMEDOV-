e4cc831a0c4005d266594c4961c957b3ebfaa1e3
Rasul <89188458381@mail.ru>
2022-07-13 10:28:48 +0300

    add 003

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3.	Даны два числа. Показать большее и меньшее число
+
+int a = 23;
+int b = 45;
+System.Console.WriteLine(Math.Max(a, b));
+System.Console.WriteLine(Math.Min(a, b));
+
