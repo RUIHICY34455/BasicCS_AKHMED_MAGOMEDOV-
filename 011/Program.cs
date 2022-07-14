80d0ddb04861c36bf4e5e5866478a84d7f3f293e
Rasul <89188458381@mail.ru>
2022-07-14 10:42:54 +0300

    add Показать вторую цифру трёхзначного числа

---
+++ 011/Program.cs
@@ -0,0 +1,12 @@
+﻿// 11.	Показать вторую цифру трёхзначного числа
+
+int a=Convert.ToInt32(Console.ReadLine());
+// if (a>=100 || a<=100)
+if(Math.Abs(a)>=100)
+{
+    System.Console.WriteLine(a%100/10);
+}
+else
+{
+    System.Console.WriteLine("Число имеет меньше трех цифр");
+}
