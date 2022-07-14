e5fa7e07a315941442a9bed3cab3637f88c061a4
Rasul <89188458381@mail.ru>
2022-07-14 10:44:00 +0300

    add Удалить вторую цифру трёхзначного числа

---
+++ 013/Program.cs
@@ -0,0 +1,9 @@
+﻿// 13.	Удалить вторую цифру трёхзначного числа
+
+int a=Convert.ToInt32(Console.ReadLine());
+int d0=a%10;
+System.Console.WriteLine(d0);
+int na=a/100;
+System.Console.WriteLine(na);
+a=na*10+d0;
+System.Console.WriteLine(a);
