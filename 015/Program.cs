b0716b70d2ac268fd697204dafc79078c2799b29
Rasul <89188458381@mail.ru>
2022-07-14 10:50:30 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ 015/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15.	Найти третью цифру числа или сообщить, что её нет
+
+int a=Convert.ToInt32(Console.ReadLine());
+// if (a>=100 || a<=100)
+if(Math.Abs(a)>=100)
+{
+    System.Console.WriteLine(a/100%10);
+}
+else
+{
+    System.Console.WriteLine("Число имеет меньше трех цифр");
+}
