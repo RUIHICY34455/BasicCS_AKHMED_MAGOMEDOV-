916d107bf4dda02cc6e2f3b7e9a16870f919e074
Rasul <89188458381@mail.ru>
2022-07-18 22:38:28 +0300

    Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0

---
+++ 020/Program.cs
@@ -0,0 +1,13 @@
+﻿// 20.	Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int Area(double x, double y)
+{
+    if(x>0 && y>0) return 1;
+    if(x<0 && y>0) return 2;
+    if(x<0 && y<0) return 3;
+    if(x>0 && y<0) return 4;
+    return 0;
+}
+
+int x=40, y=40;
+System.Console.WriteLine(Area(x, y));
