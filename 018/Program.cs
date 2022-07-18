52a9f7f3e7f8d94cabd1d0eb1676480e4a23219d
Rasul <89188458381@mail.ru>
2022-07-18 19:51:15 +0300

    По двум заданным числам проверять является ли одно квадратом другого. Используйте подпрограмму

---
+++ 018/Program.cs
@@ -0,0 +1,17 @@
+﻿// 18. По двум заданным числам проверять является ли одно квадратом другого. Используйте подпрограмму
+
+bool Check(int a, int b)
+{
+    return a*a==b || b*b==a;
+}
+
+int a=3;
+int b=9;
+if (Check(a, b))
+{
+    System.Console.WriteLine("Является");
+}
+else
+{
+    System.Console.WriteLine("Не является");
+}
