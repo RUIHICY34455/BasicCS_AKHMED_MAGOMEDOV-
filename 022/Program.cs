49381b73f77d8213504d53b3ffe92fd8cf652faf
Rasul <89188458381@mail.ru>
2022-07-18 23:47:49 +0300

    Программа проверяет пятизначное число на палиндромом

---
+++ 022/Program.cs
@@ -0,0 +1,29 @@
+﻿// 22. Программа проверяет пятизначное число на палиндромом
+
+int Palindrome;
+System.Console.WriteLine("Введите пятизначное чило ");
+
+int num;
+num=int.Parse(Console.ReadLine());
+
+int a=num%10;
+int b=num%100/10;
+int c=num/10000;
+int d=num/1000%10;
+
+bool Palindrome=a==c && b==d;
+
+if(Palindrome) 
+{
+    System.Console.WriteLine("Число является палинтромом!");
+}
+    
+else 
+{
+    System.Console.WriteLine("Число не является палинтромом!");
+}
+    
+   
+
+
+System.Console.WriteLine($"{a} {b} {c} {d} ");
