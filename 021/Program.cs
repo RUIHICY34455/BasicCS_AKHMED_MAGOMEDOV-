17aa502477cd1614d3bffca08fb330cb8278f1c4
Rasul <89188458381@mail.ru>
2022-07-18 22:49:54 +0300

    Задать номер четверти, показать диапазоны для возможных координат

---
+++ 021/Program.cs
@@ -0,0 +1,24 @@
+﻿// 21. Задать номер четверти, показать диапазоны для возможных координат
+
+int num;
+
+num=int.Parse(Console.ReadLine());
+
+switch(num)
+{
+    case 1:
+        System.Console.WriteLine("x=от 0 до +Ꝏ, y=от 0 до +Ꝏ");
+        break;
+    case 2:
+        System.Console.WriteLine("x=от -Ꝏ до 0, y=от 0 до +Ꝏ");
+        break;
+    case 3:
+        System.Console.WriteLine("x=от -Ꝏ до 0, y=от -Ꝏ до 0");
+        break;
+    case 4:
+        System.Console.WriteLine("x=от 0 до +Ꝏ, y=от -Ꝏ до 0");
+        break;
+    default:
+        System.Console.WriteLine("wrong");
+        break;
+}
