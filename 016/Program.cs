badee78e968e560a8f8233e6a84adbb286d00acd
Rasul <89188458381@mail.ru>
2022-07-18 19:33:46 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ 016/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a=Convert.ToInt32(Console.ReadLine()); 
+bool b=a%7==0 && a%23==0; 
+//System.Console.WriteLine(b); 
+if (b) 
+{     System.Console.WriteLine("Кратно"); }
+else {     System.Console.WriteLine("Не кратно"); }
