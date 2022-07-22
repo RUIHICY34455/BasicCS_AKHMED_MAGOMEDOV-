ef08661458c517d0ab68bb3cdb795b02f5bb38e5
Rasul <89188458381@mail.ru>
2022-07-22 15:38:15 +0300

    Задать массив из 8 целых элементов и вывести их на экран

--- 032/Program.cs
+++ 032/Program.cs
@@ -3,10 +3,11 @@
 int N=8;
 
 int[] a=new int[N];
+Random random = new Random();
 for(int i=0; i<a.Length;i++)
-    a[i]=1;
+    a[i]=random.Next(0,100);
 for(int i=0; i<a.Length;i++)
-    System.Console.Write(a[i]);
+    System.Console.Write($"{a[i],2}");
 
 
 
