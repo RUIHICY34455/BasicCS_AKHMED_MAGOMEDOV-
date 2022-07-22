6287652069e89f78d1ac08fa8a1d7a55946bd62a
Rasul <89188458381@mail.ru>
2022-07-22 17:17:29 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ 031/Program.cs
@@ -0,0 +1,11 @@
+﻿// 31. Показать кубы чисел, заканчивающихся на четную цифру
+
+int N=8;
+int i=1;
+while(i<=N)
+{
+    int a=3;
+    double sum = Math.Pow(i,a);
+    if(sum%2==0) System.Console.WriteLine(sum);
+    i++;
+}
