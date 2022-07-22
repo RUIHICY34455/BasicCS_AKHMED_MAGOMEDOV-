ffd9cb2ab7fe9f08301d7cd536d242c0f733ac16
Rasul <89188458381@mail.ru>
2022-07-22 16:29:34 +0300

    Возведите число А в натуральную степень B используя цикл

---
+++ 027/Program.cs
@@ -0,0 +1,15 @@
+﻿// 27. Возведите число А в натуральную степень B используя цикл
+
+int A=8;
+int B=5;
+int i=1;
+int sum=1; 
+
+while(i<=B)
+{
+    sum=sum*A;
+    i++;
+}
+
+
+System.Console.WriteLine(sum);
