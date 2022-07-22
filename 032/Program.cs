08d29e2fd37886a1e2d425a25f45a3cc821e278a
Rasul <89188458381@mail.ru>
2022-07-22 15:15:52 +0300

    Задать массив из 8 целых элементов и вывести их на экран

---
+++ 032/Program.cs
@@ -0,0 +1,35 @@
+﻿// 32. Задать массив из 8 целых элементов и вывести их на экран
+
+int N=8;
+
+int[] a=new int[N];
+for(int i=0; i<a.Length;i++)
+    a[i]=1;
+for(int i=0; i<a.Length;i++)
+    System.Console.Write(a[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* значимые типы (value type)
+int a; 
+double b;
+float c;
+bool d;
+
+// ссылочные типы (reference type)
+string s;
+int[] ar;
+Random random;
+*/
