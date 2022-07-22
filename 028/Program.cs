d1647dc1187befeff4e5c8498669f7c8a43e648a
Rasul <89188458381@mail.ru>
2022-07-22 14:55:19 +0300

    Определить количество цифр в числе. Сделать подпрограмму

---
+++ 028/Program.cs
@@ -0,0 +1,32 @@
+﻿// 28. Определить количество цифр в числе. Сделать подпрограмму.
+
+System.Console.WriteLine(CountDigits(785469));
+
+
+
+int CountDigits(int N)
+{
+    if(N==0) return 1;
+    int k=0;
+    while(N!=0)
+    {
+        k++;
+        N/=10;
+    }
+    return k;
+}
+
+
+
+
+/*int N=1234;
+int k=0;
+while(N!=0)
+{
+    k++;
+    //N=N/10;
+    N/=10;
+}
+
+System.Console.WriteLine(k);
+*/
