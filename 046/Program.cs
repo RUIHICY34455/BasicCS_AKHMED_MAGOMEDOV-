33f302d0b321198e6dcc65e540050e77227f8b50
Rasul <89188458381@mail.ru>
2022-08-12 15:53:35 +0300

    С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1

---
+++ 046/Program.cs
@@ -0,0 +1,28 @@
+﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+System.Console.WriteLine("Введите число: ");
+
+int num;
+num=int.Parse(Console.ReadLine());
+
+int fib1 = 0;
+int fib2 = 1;
+int fib_sum;
+fib_sum=0;
+
+int i=0;
+while (i<num)
+{
+    fib_sum = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = fib_sum;
+    System.Console.WriteLine($"{fib1} {fib2}");
+    i++;
+}
+/*for(int i=0;i<num;i++)
+
+    fib_sum = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = fib_sum;
+    System.Console.WriteLine($"{fib1} {fib2}");
+    */
