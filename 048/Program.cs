00e7cb258c0a625203e61211a12501d75259e179
Rasul <89188458381@mail.ru>
2022-08-12 18:00:56 +0300

    Написать программу копирования массива

---
+++ 048/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу копирования массива
+
+int N=9;
+
+int[] a=new int[N];
+Random random = new Random();
+
+for(int i=0; i<a.Length;i++)
+    a[i]=random.Next(0,10);
+
+
+int[] b=new int[a.Length];
+
+Array.Copy(a, b, a.Length);
+
+for(int i=0; i<a.Length;i++)
+System.Console.Write($"{a[i]} {b[i]}  ");
