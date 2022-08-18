bb2ea4c77ad1150f2c28647944bdad339644e7dd
Rasul <89188458381@mail.ru>
2022-08-18 10:24:23 +0300

    Написать программу вычисления функции Аккермана

---
+++ FunctionAkerman/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления функции Аккермана
+
+
+static int A(int n, int m)
+{
+    if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+    if (n == 0) return m + 1;
+    if (m == 0) return A(n - 1, m);
+    return A(n - 1, A(n, m - 1));
+}
+System.Console.WriteLine(A(0,2));
+
